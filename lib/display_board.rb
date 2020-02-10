44a48d217d891a3b721400d525300bdfdd177368
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-10 02:55:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts" | | "
+  puts"   |   |   "
   puts"-----------"
-  puts" O|X| "
+  puts" O | X |   "
   puts"-----------"
-  puts" | | "
+  puts"   |   |   "
 end
