7aa29cbd970efa735bf0557e31d52ed8cbc3bf0e
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-10 03:26:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  separator ="|"
+  separator = "|"
   lines = "-----------"
   puts "#{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
   puts "#{lines}"
